4e6bfa261875af3cce07b4a9c702594e36264edc
El khalil <elkhalil.zbir@gmail.com>
2023-12-14 00:40:49 +0100

    fix

--- memory.c
+++ memory.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * push - push a pointer into an array of pointers
- * @arr: pointer to array of pointers
- * @length: pointer to length of current array
- * @ptr: pointer to push to array
- * Return: 1 on success, 0 on failure
- */
+* push - push a pointer into an array of pointers
+* @arr: pointer to array of pointers
+* @length: pointer to length of current array
+* @ptr: pointer to push to array
+* Return: 1 on success, 0 on failure
+*/
 int push(void ***arr, size_t *length, void *ptr)
 {
 	void **curr = *arr,
+
 		 **res = malloc((*length + 1) * sizeof(void *));
 	size_t i;
 
@@ -26,15 +27,16 @@ int push(void ***arr, size_t *length, void *ptr)
 }
 
 /**
- * _realloc - reallocates memory and attempts to change its size
- * @ptr: ptr to allocated memeory
- * @old_size: old size of buffer
- * @new_size: size of buffer after resize
- * Return: pointer to newly allocated memory
- */
+* _realloc - reallocates memory and attempts to change its size
+* @ptr: ptr to allocated memeory
+* @old_size: old size of buffer
+* @new_size: size of buffer after resize
+* Return: pointer to newly allocated memory
+*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i, smaller_size = old_size < new_size ? old_size : new_size;
+
 	char *ptr_c = ptr, *res;
 
 	if (old_size == new_size)
@@ -66,10 +68,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 }
 
 /**
- * my_free - frees a memory block and sets it to null, only if it's not null
- * @ptr: pointer to a pointer that points to the memory block
- * Return: 1 if freed, 0 if not freed
- */
+* my_free - frees a memory block and sets it to null, only if it's not null
+* @ptr: pointer to a pointer that points to the memory block
+* Return: 1 if freed, 0 if not freed
+*/
 int my_free(void **ptr)
 {
 	if (*ptr == NULL)
